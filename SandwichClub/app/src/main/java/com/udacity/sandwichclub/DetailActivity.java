0954d800eebb176fdf69e2240fbb7bb3c0981e47
kholoudaliit <kholoud.ali.it@gmail.com>
2019-09-06 17:07:16 +0300

    Added the UI stuff in Detail page and MainActivity

--- SandwichClub/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ SandwichClub/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.udacity.sandwichclub;
 
+import android.app.MediaRouteButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -20,14 +24,15 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = 0;
+        if (intent != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        }
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -43,12 +48,8 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+        populateUI(sandwich);
 
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +57,65 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        TextView SanName = findViewById(R.id.sanName);
+        TextView SanOthersName = findViewById(R.id.also_known_tv);
+        TextView SanOthersNameLBL = findViewById(R.id.aslo_known_lbl);
+        TextView SanSIngredent = findViewById(R.id.ingredients_tv);
+        TextView SanSIngredentLBL = findViewById(R.id.ingrdients_lbl);
+        TextView SanOriginPlace = findViewById(R.id.placeOfOrigin);
+        TextView SanOriginPlaceLBL = findViewById(R.id.place_lbl);
+        TextView SanDesciption = findViewById(R.id.description_tv);
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar_detail);
+
+        // Set the sandwich image
+        progressBar.setVisibility(View.VISIBLE);
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError() {
+                    }
+                });
+
+        //Set Activity title
+        setTitle(sandwich.getMainName());
+        //Set Names
+        SanName.setText(sandwich.getMainName());
+        if (!sandwich.getAlsoKnownAs().isEmpty()) {
+            String othersName = String.join(" | ", sandwich.getAlsoKnownAs());
+            SanOthersName.setText(othersName);
+        } else {
+            SanOthersNameLBL.setVisibility(View.INVISIBLE);
+            SanOthersName.setVisibility(View.INVISIBLE);
+        }
+
+        //Set Origin Place
+        if (! sandwich.getPlaceOfOrigin().isEmpty()) {
+            SanOriginPlace.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            SanOriginPlace.setVisibility(View.INVISIBLE);
+            SanOriginPlaceLBL.setVisibility(View.INVISIBLE);
+        }
+
+        //Set Description
+        SanDesciption.setText(sandwich.getDescription());
+
+        //Set ingredients
+        if (!sandwich.getIngredients().isEmpty()) {
+            String ingredient = String.join(" \n ", sandwich.getIngredients());
+            SanSIngredent.setText(ingredient);
+        } else {
+            SanSIngredentLBL.setVisibility(View.INVISIBLE);
+            SanSIngredent.setVisibility(View.INVISIBLE);
+        }
 
     }
 }
--- SandwichClub/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ SandwichClub/app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -1,12 +1,26 @@
 package com.udacity.sandwichclub;
 
+import android.app.MediaRouteButton;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.model.Sandwich;
+import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
+        //get the full sandwiches list
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        List<Sandwich> sandwichList = new ArrayList<>();
+        // fill the pares sandwich obj to list
+        for (String sandwichJson :
+                sandwiches) {
+            Sandwich sandwich = JsonUtils.parseSandwichJson(sandwichJson);
+            sandwichList.add(sandwich);
+        }
+        // custom SandwichAdapter
+        SandwichAdapter adapter = new SandwichAdapter(this, sandwichList);
 
         // Simplification: Using a ListView instead of a RecyclerView
         ListView listView = findViewById(R.id.sandwiches_listview);
@@ -34,5 +56,51 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.EXTRA_POSITION, position);
         startActivity(intent);
+    }
+
+
+}
+
+class SandwichAdapter extends ArrayAdapter<Sandwich> {
+    protected SandwichAdapter(Context context, List<Sandwich> sandwiches) {
+        super(context, 0, sandwiches);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Sandwich sandwich = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        // Lookup view for data population
+        ImageView sanImage = convertView.findViewById(R.id.sandwich_img);
+        final ProgressBar progressBar = convertView.findViewById(R.id.progressBar);
+        TextView sanName = convertView.findViewById(R.id.san_name);
+        TextView sanHome = convertView.findViewById(R.id.san_place);
+        /* Populate the data into the template view using the data object */
+
+        //load image URL using Picasso
+        // Show progress bar
+        progressBar.setVisibility(View.VISIBLE);
+        if (sandwich.getImage() != null) {
+            Picasso.with(getContext()).load(sandwich.getImage()).into(sanImage, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    if (progressBar != null) {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+
+                @Override
+                public void onError() {
+                }
+            });
+        }
+        sanName.setText(sandwich.getMainName());
+        sanHome.setText(sandwich.getPlaceOfOrigin());
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
