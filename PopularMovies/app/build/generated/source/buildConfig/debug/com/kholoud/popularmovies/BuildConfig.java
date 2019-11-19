db8552efb7caa69dc33bc551737cb6d388caa888
kholoudaliit <kholoud.ali.it@gmail.com>
2019-11-19 18:56:44 +0300

    added bakingApp

---
+++ PopularMovies/app/build/generated/source/buildConfig/debug/com/kholoud/popularmovies/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kholoud.popularmovies;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kholoud.popularmovies";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String moviedb_apikey = "30c2acf430f2f6482a56f2124348f09c";
+}
